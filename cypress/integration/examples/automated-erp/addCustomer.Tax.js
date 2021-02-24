4c68e6a033c645c00a3538d9e8831c3a88b63a4c
nam <nam.jirawan@icloud.com>
2021-02-24 16:55:46 +0700

    24.2.64

--- cypress/integration/examples/automated-erp/addCustomer.Tax.js
+++ cypress/integration/examples/automated-erp/addCustomer.Tax.js
@@ -2,23 +2,23 @@
 
 const viewPorts = ["macbook-16"]
 viewPorts.map(viewPort => {
-    context("add-customer", () => {
+    context("add-customer-tax", () => {
         beforeEach(() => {
             cy.viewport(viewPort)
             cy.visit("https://smdevdemo.autocareth.com/retailer/home")
         })
-        it("customer-" + viewPort, () => {
+        it("customer-Tax" + viewPort, () => {
             cy.get('#input_username').type("retail-CRR")
             cy.get('#input_password').type("password")
             cy.get('.btn').click({force: true})
             cy.get(':nth-child(5) > .col-12').click({force: true})
             const paymentTypes = ["เงินสด","เครดิตเทอม","โอน","เช็ค"]
-            paymentTypes.map(paymentType => {
+            paymentTypes.map(paymentType => {   
                 cy.get('#customerdesk > .d-xl-flex > .text-right > a > .btn')
                 .invoke('attr', 'style', 'display: ')
                 .should('have.attr', 'style', 'display: ')
                 cy.get('#customerdesk > .d-xl-flex > .text-right > a > .btn').click()
-                cy.get(':nth-child(2) > .row > :nth-child(1) > .form-control').type("jirawan")
+                cy.get(':nth-child(2) > .row > :nth-child(1) > .form-control').type("jirawan 1")
                 cy.get(':nth-child(3) > .row > :nth-child(1) > .form-control').type("phahon 54 168/106")
                 cy.get(':nth-child(3) > .row > :nth-child(2) > .form-control').type("0888888888")
                 cy.get(':nth-child(4) > .row > :nth-child(1) > .form-control').type("0123456789012")
---
+++ cypress/integration/examples/automated-erp/addDealer.Tax.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+context("add-dealer-Tax", () => {
+    beforeEach(() => {
+        cy.visit("https://smdevdemo.autocareth.com/retailer/home")
+    })
+    it("dealer-Tax", () => {
+        cy.get('#input_username').type("retail-CRR")
+        cy.get('#input_password').type("password")
+        cy.get('.btn').click({ force: true })
+        cy.get(':nth-child(5) > .col-12').click({ force: true })
+        const paymentType = ["เงินสด", "โอน", "เช็ค", "เครดิตเทอม"]
+        paymentType.map(paymentType => {
+            cy.get(':nth-child(2) > .nav-link')
+                .invoke('attr', 'style', 'display: ')
+                .should('have.attr', 'style', 'display: ')
+            cy.get(':nth-child(2) > .nav-link').click({ force: true })
+            cy.get('#distributordesk > .d-flex > .col-lg-12 > a > .btn').click({ force: true })
+            cy.get('#distributordesk > .d-flex > .col-lg-12 > a > .btn').click({ force: true })
+            cy.get(':nth-child(2) > .row > :nth-child(1) > .form-control').type("jirawan 2")
+            cy.get(':nth-child(3) > .row > :nth-child(1) > .form-control').type("phahon54 168/106")
+            cy.get(':nth-child(4) > .row > :nth-child(1) > .form-control').type("0123456789012")
+            cy.get(':nth-child(2) > .row > :nth-child(2) > .form-control').type("nam 2")
+            cy.get(':nth-child(3) > .row > :nth-child(2) > .form-control').type("0955915150")
+            cy.get(':nth-child(5) > .row > :nth-child(1) > .form-control').select(paymentType)
+            cy.get(':nth-child(2) > .btn').click({ force: true })
+            cy.get('.swal2-confirm').click({ force: true })
+        })
+    })
+})
