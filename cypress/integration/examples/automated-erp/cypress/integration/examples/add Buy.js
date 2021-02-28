6e991b6ee808a989b01343ab4d610bcdb2378590
nam <nam.jirawan@icloud.com>
2021-02-28 16:31:34 +0700

    exampleExpect

--- cypress/integration/examples/automated-erp/cypress/integration/examples/add Buy.js
+++ cypress/integration/examples/automated-erp/cypress/integration/examples/add Buy.js
@@ -2,7 +2,7 @@
 
 // const { without } = require("cypress/types/lodash")
 
-Cypress.config('defaultCommandTimeout', 100000)
+Cypress.config('defaultCommandTimeout', 10000)
 context("add-buy-Tax", () => {
     beforeEach(() => {
         cy.visit("https://smdevdemo.autocareth.com/retailer/home")
@@ -16,11 +16,37 @@ context("add-buy-Tax", () => {
         cy.get(':nth-child(1) > td > .btn').click({ force: true })
         cy.get('#addProductModal > .modal-dialog > .modal-content > .modal-header > .close > span').click()
         AddBuy()
-        Dats()
+        Data()
+        tax()
     })
 })
 
+// วิธีเช็ค
+const exampleExpect = () => {
+    
+    // cy.get('strong').then(rs => {
+    //     // console.log(rs);
+    //     expect(rs[0].outerHTML)
+    //         .eq('<strong data-v-9fea38fc="">สำหรับ Retailer และ Supplier</strong>')
+    // })
+    
+    // cy.get('strong').contains("สำหรับ Retailer และ Supplier")
+    
+    // cy.get('strong').should("contain.text", "สำหรับ Retailer และ Supplier")
+    // const customer = {
+    //     name: "in",
+    //     age: 26
+    // }
+    // cy.wrap(customer).its("name").should("eq", "in")
+    // cy.wrap(customer).its("age").should("eq", 26)
+    // cy.wrap(customer).should("deep.equal", {
+    //     name: "in",
+    //     age: 26
+    // })
+}
+
 const login = (username, password) => {
+    // exampleExpect()
     cy.get('#input_username').type(username)
     cy.get('#input_password').type(password)
     cy.get('.btn').click()
@@ -37,20 +63,50 @@ const AddBuy = () => {
     cy.get('.text-right > .btn-confirm').click()
 }
 
+// ข้อมูลจำนวนชิ้น,ราคา และนำตัวแปลไปใส่แทนที่ตัวเลข
+const Data = () => {
+    const products = [
+        {
+            price: 100,
+            qty: 3,
+            percentage1: 5,
+            percentage2: 5
+        },
+        {
+            price: 200,
+            qty: 2,
+            percentage1: 5,
+            percentage2: 5,
+        }
+    ]
+    cy.get('#products-0 > :nth-child(2) > .form-row > :nth-child(1) > .el-input > .el-input__inner').clear().type(products[0].qty)
+    cy.get('#products-0 > [style="width: 200px;"] > :nth-child(1) > .text-left > .form-control').type(products[0].percentage1)
+    cy.get('#products-0 > [style="width: 200px;"] > :nth-child(2) > .text-left > .form-control').type(products[0].percentage2)
+    cy.get('#products-0 > [style="width: 150px;"] > .el-input > .el-input__inner').type(products[0].price)
+    cy.get('#products-1 > :nth-child(2) > .form-row > :nth-child(1) > .el-input > .el-input__inner').clear().type(products[1].qty)
+    cy.get('#products-1 > [style="width: 200px;"] > :nth-child(1) > .text-left > .form-control').type(products[1].percentage1)
+    cy.get('#products-1 > [style="width: 200px;"] > :nth-child(2) > .text-left > .form-control').type(products[1].percentage2)
+    cy.get('#products-1 > [style="width: 150px;"] > .el-input > .el-input__inner').type(products[1].price)
+    cy.get('.col-md-5').click({ force: true })
 
-const Dats = () => { 
-    cy.get('#products-0 > :nth-child(2) > .form-row > :nth-child(1) > .el-input > .el-input__inner').clear().type("3")
-    cy.get('#products-0 > [style="width: 200px;"] > :nth-child(1) > .text-left > .form-control').type("5")
-    cy.get('#products-0 > [style="width: 200px;"] > :nth-child(2) > .text-left > .form-control').type("5")
-    cy.get('#products-0 > [style="width: 150px;"] > .el-input > .el-input__inner').type("100")
-    cy.get('#products-1 > :nth-child(2) > .form-row > :nth-child(1) > .el-input > .el-input__inner').clear().type("2")
-    cy.get('#products-1 > [style="width: 200px;"] > :nth-child(1) > .text-left > .form-control').type("10")
-    cy.get('#products-1 > [style="width: 200px;"] > :nth-child(2) > .text-left > .form-control').type("10")
-    cy.get('#products-1 > [style="width: 150px;"] > .el-input > .el-input__inner').type("200")
-    cy.get('.col-md-5 > :nth-child(1) > :nth-child(1) > .form-control').click()
-    //ToDo เขียนตรวจสอบ ควรจะเป็นอย่างไร ตัวเลขต้องถูกต้อง
-    cy.get('.col-sm-12.p-0 > [style="background-color: rgb(243, 244, 246);"] > .form-row > .col-md-8 > .el-select > .el-input > .el-input__inner')
-    .type("{downarrow}{downarrow}{enter}")
+    // วิธีหาผลลัพธ์ ราคาและเปอร์เซ็น
+    let totalPrice = 0
+    products.map(product => {
+        totalPrice += product.qty * product.price
+    })
+    // วิธีเช็ค 
+    totalPrice = totalPrice.toFixed(2)
+    // แบบที่1
+    // cy.get(':nth-child(1) > .row > .text-right > h5').should("contain.text", totalPrice)
+    
+    // แบบที่2
+    cy.get(':nth-child(1) > .row > .text-right > h5').should(rs => {
+        expect(rs[0].outerHTML).eq('<h5 data-v-43e18a8c=""> '+ totalPrice +' บาท</h5>')
+    })
 }
 
-// cy.get(':nth-child(4) > :nth-child(1) > .el-autocomplete > .el-input > .el-input__inner').click().tab().type("{downarrow}{downarrow}{downarrow}{enter}")
+const tax = () => {
+    cy.get('.col-sm-12.p-0 > [style="background-color: rgb(243, 244, 246);"] > .form-row > .col-md-8 > .el-select > .el-input > .el-input__inner').click()
+        .type("{downarrow}{downarrow}{enter}")
+}
+
