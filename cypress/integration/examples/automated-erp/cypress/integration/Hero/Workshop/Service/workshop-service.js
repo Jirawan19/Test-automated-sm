b9ca4cd3e2a07c3a99d48da7f97e8b11e51d06d8
jirawan19~ <nam.jirawan@icloud.com>
2021-05-19 20:52:55 +0700

    workshop-service

---
+++ cypress/integration/examples/automated-erp/cypress/integration/Hero/Workshop/Service/workshop-service.js
@@ -0,0 +1,72 @@
+/// <reference types="cypress" />
+
+Cypress.config('defaultCommandTimeout', 100000)
+
+context("Service", () => {
+    beforeEach(() => {
+        cy.visit("https://herodemo.autopair.co/")
+    })
+    it("Add Service", () => {
+        loginWorkshop("empGrip01", "password")
+        AddService()
+        checkservice()
+    })
+})
+const loginWorkshop = (username, password) => {
+    cy.get('.my-4 > .text-left > span').should("contain.text", "ชื่อผู้ใช้งาน")
+    cy.get('#username').type(username)
+    cy.get('.mb-3 > .text-left > span').should("contain.text", "รหัสผ่าน")
+    cy.get('#password').type(password)
+    cy.get('.btn-global').click()
+}
+
+// เข้ารายการ บริการ
+const AddService = () => {
+    cy.get('.CardheadTitle > h3').should("contain.text", "รายการซื้อ")
+    cy.get(':nth-child(6) > .nav-link > .row > h6').click()
+
+    // เพิ่มบริการ
+    cy.get('h3[data-v-56f9da8c=""]').should("contain.text", "บริการ")
+    cy.get('.el-button').click()
+
+    // รายละเอียดบริการ
+    cy.get('h3').should("contain.text", "เพิ่มบริการ")
+    cy.get(':nth-child(1) > .form-label').should("contain.text", "ชื่อบริการ")
+    taxAddService(getRandomAddService(1, 3))
+    cy.get(':nth-child(2) > .form-label').should("contain.text", "ราคาบริการ")
+    cy.get('#__BVID__69').type("150")
+    cy.get('.col-lg-12 > .form-label').should("contain.text", "รายละเอียดบริการ")
+    taxAddService1(getRandomAddService(1, 3))
+
+    cy.get(':nth-child(2) > .btn').should("contain.text", "เพิ่มบริการ")
+    cy.get(':nth-child(2) > .btn').click()
+
+    cy.get('#swal2-title').should("contain.text", "เพิ่มบริการสำเร็จ")
+    cy.get('.swal2-confirm').should("contain.text", "ตกลง")
+    cy.get('.swal2-confirm').click()
+
+}
+
+const checkservice = () => {
+    cy.get('h3[data-v-56f9da8c=""]').should("contain.text", "บริการ")
+    cy.get('#__BVID__73__BV_label_').should("contain.text", "ค้นหาบริการ")
+    cy.get('#__BVID__74').click().type("test")
+    cy.get('#__BVID__73__BV_label_').click()
+    cy.get('#__BVID__73__BV_label_').click()
+    cy.get('#__BVID__74').click()
+
+}
+
+const getRandomAddService = (min, max) => {
+    0, 0
+    return Math.random() * (max - min) + min;
+}
+const taxAddService = (textNo) => {
+    cy.get('#__BVID__68')
+        .type("test Service").type(textNo)
+}
+const taxAddService1 = (textNo) => {
+    cy.get('.el-textarea__inner')
+        .type("test Service").type(textNo)
+}
+
